c8252db4640c4fd2af79657f852342d51571bd28
21bca58 <96609503+21bca58@users.noreply.github.com>
2022-03-10 11:16:41 +0530

    Add files via upload

---
+++ solution/evennum.py
@@ -0,0 +1,6 @@
+#was enter any number and check its even or odd
+a=int(input("Enter any number"))
+if(a%2==0):
+    print("{0} is even number:".format(a))
+else:
+    print("{0} is odd:".format(a))
---
+++ solution/palindrone.py
@@ -0,0 +1,12 @@
+# was to number of pailndrone
+n=int(input("Enter number:"))
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
+    print("The number is a palindrome")
+else:
+    print("not palindrome")
